3a091f2640855b79e35828b91136431a2498e7b9
darman1725 <darmansaragih1701@gmail.com>
2021-10-10 16:38:07 +0700

    Membuat 3 Method Untuk Assign "Celcius"

--- app/src/main/java/org/aplas/basicappx/Temperature.java
+++ app/src/main/java/org/aplas/basicappx/Temperature.java
@@ -6,4 +6,17 @@ public class Temperature {
     Temperature(){
         this.celcius = 0;
     }
+
+    public void setCelcius(double celci){
+        this.celcius = celci;
+    }
+    public void setFahrenheit(double farenheit){
+        this.celcius = convert("°F", "°C", farenheit);
+    }
+    public void setKelvins(double kelvin){
+        this.celcius = convert("K", "°C", kelvin);
+    }
+
+
 }
+
