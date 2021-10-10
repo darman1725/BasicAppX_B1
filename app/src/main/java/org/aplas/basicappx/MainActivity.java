7863e972f19a25031f9935c2852ec34f3bbbebb9
darman1725 <darmansaragih1701@gmail.com>
2021-10-10 19:09:13 +0700

    Membuat Beberapa Atribut

--- app/src/main/java/org/aplas/basicappx/MainActivity.java
+++ app/src/main/java/org/aplas/basicappx/MainActivity.java
@@ -34,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
     private ImageView imgView;
     private AlertDialog startDialog;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
