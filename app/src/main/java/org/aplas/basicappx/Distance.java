b3c19f9940c58875c8009337eb93e3df0fe16b42
darman1725 <darmansaragih1701@gmail.com>
2021-10-10 17:23:49 +0700

    Menambahkan 4 Method Untuk assign “meter”

--- app/src/main/java/org/aplas/basicappx/Distance.java
+++ app/src/main/java/org/aplas/basicappx/Distance.java
@@ -7,4 +7,20 @@ public class Distance {
         meter = 0;
     }
 
+    public void setMeter(double meter){
+        this.meter = meter;
+    }
+
+    public void setInch(double meter){
+        this.meter = convert("Inc", "Mtr", meter);
+    }
+
+    public void setMile(double meter){
+        this.meter = convert("Mil", "Mtr", meter);
+    }
+
+    public void setFoot(double meter){
+        this.meter = convert("Ft", "Mtr", meter);
+    }
+
 }
