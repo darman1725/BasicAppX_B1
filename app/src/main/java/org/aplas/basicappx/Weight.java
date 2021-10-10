62c42adbff7689896ff50992c8eada66c8543d88
darman1725 <darmansaragih1701@gmail.com>
2021-10-10 17:33:17 +0700

    Membuat 3 Method Untuk Assign "Gram"

--- app/src/main/java/org/aplas/basicappx/Weight.java
+++ app/src/main/java/org/aplas/basicappx/Weight.java
@@ -7,4 +7,17 @@ public class Weight {
         gram = 0;
     }
 
+    public void setGram(double gram){
+        this.gram = gram;
+    }
+
+    public void setOunce(double gram){
+        this.gram = convert("Onc", "Grm", gram);
+    }
+
+    public void setPound(double gram){
+        this.gram = convert("Pnd", "Grm", gram);
+    }
+
+
 }
