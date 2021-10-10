363f31d50b0520c6d5d51ced42ae5847b20b79ff
darman1725 <darmansaragih1701@gmail.com>
2021-10-10 19:08:51 +0700

    Membuat Beberapa Atribut

--- app/src/main/java/org/aplas/basicappx/MainActivity.java
+++ app/src/main/java/org/aplas/basicappx/MainActivity.java
@@ -1,9 +1,39 @@
 package org.aplas.basicappx;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
+    private Distance dist = new Distance();
+    private Weight weight = new Weight();
+    private Temperature temp = new Temperature();
+    private Button convertBtn;
+    private EditText inputTxt;
+    private EditText outputTxt;
+    private Spinner unitOri;
+    private Spinner unitConv;
+    private RadioGroup unitType;
+    private CheckBox roundBox;
+    private CheckBox formBox;
+    private ImageView imgView;
+    private AlertDialog startDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
